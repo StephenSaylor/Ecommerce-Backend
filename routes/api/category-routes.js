bfe8021859e1bc75b825ab263834aabd995cd264
Jeff Lebowski <anentitymusic@gmail.com>
2021-05-20 16:40:59 -0400

    product get routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,10 +7,8 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll({
-    include: {
-      model: Product,
-      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
-    }
+    include: { model: Product },
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
   })
   .then(catData => {
     if (!catData) {
@@ -32,10 +30,8 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id
     },
-    include: {
-      model: Product,
-      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
-    } 
+    include: { model: Product },
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
   })
   .then(catData => {
     if (!catData) {
